cdc9f641c31b01f6496ff708c7bfb31346a01f48
Fang Lin <fanglin@uw.edu>
2024-02-17 19:18:31 -0800

    python 3.10

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     name='aws-message-client',
     version=VERSION,
     packages=['aws_message'],
+    author="UW-IT T&LS",
+    author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires = [
         'boto3',
@@ -42,6 +44,5 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
     ],
 )
